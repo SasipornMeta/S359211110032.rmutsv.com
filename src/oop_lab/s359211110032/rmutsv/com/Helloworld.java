2256485a37aa8e512192d500ac75621e46c6ffaf
Sasiporn <Sasiporn409@gmail.com>
2017-11-20 14:03:15 +0700

    OOP Lab 2

---
+++ src/oop_lab/s359211110032/rmutsv/com/Helloworld.java
@@ -0,0 +1,15 @@
+package oop_lab.s359211110032.rmutsv.com;
+
+public class Helloworld {
+
+    //    Create main method
+    public static void main(String[] args) {
+
+        System.out.println("Hello world, I'm gift");
+        System.out.println("My name is Sasiporn Metaweesuwan");
+
+//        Add infomation
+        System.out.println("Faculty: Management Technology, RMUTSV");
+
+    }   // main
+}   //class
