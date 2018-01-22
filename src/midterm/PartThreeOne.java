63b3b2a92cf33fbb25d425c5faf5bf513d421cb2
Sasiporn <33408853+SasipornMeta@users.noreply.github.com>
2018-01-22 13:46:40 +0700

    Update PartThreeOne.java

--- src/midterm/PartThreeOne.java
+++ src/midterm/PartThreeOne.java
@@ -9,18 +9,18 @@ public class PartThreeOne {
         BufferedReader reader = new BufferedReader
                 (new InputStreamReader(System.in));
 
-        int count = 0;
+        int count = 1;
         int birthday = 0;
         int year = 2561;
 
         for (int i=1;i<=3;i++){
-            System.out.print("Enter Year: ");
+            System.out.print("Enter Year " +count+ " :");
             birthday = Integer.parseInt(reader.readLine());
 
             int Age = year-birthday;
             System.out.println("Age : "+Age);
                 count++;
-                if (count == 3) {
+                if (count == 4) {
                     System.out.println("Thank you.");
                     break;
                 }
